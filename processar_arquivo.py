8b228b8e3fedbf7f07ab6ed779226a6d4b7d8289
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2024-10-08 20:23:46 -0300

    Adaptando arquivo para converter um texto numa unica coluna num csv, com 3 colunas baseado num padrao de texto

--- processar_arquivo.py
+++ processar_arquivo.py
@@ -3,7 +3,7 @@ import os
 def processar_arquivo(arquivo_origem,arquivo_destino):
 	try:
 		with open(arquivo_origem,'r',encoding='utf-8') as f_origem:
-			conteudo = f_origem.read()
+			conteudo = [linha.rstrip() for linha in f_origem]
 	except FileNotFoundError:
 		print(f'Arquivo {arquivo_origem} não encontrado')
 		return
@@ -14,9 +14,17 @@ def processar_arquivo(arquivo_origem,arquivo_destino):
 		print(f'Erro Inesperado ao ler {arquivo_origem}:{e}')
 		return
 	try:
+		texto = ''
+		for i in range(len(conteudo)):
+		
+			if (i+1)%3 == 0:
+				texto = texto + conteudo[i] + '\n'
+			else:			
+				texto = texto + conteudo[i]+ ';'	
+						
+
 		with open(arquivo_destino,'w',encoding= 'utf-8') as f_destino:
-			f_destino.write('Cabeçalho: Conteudo do Arquivo\n')
-			f_destino.write(conteudo)
+			f_destino.write(texto)
 			print(f'Conteudo escrito em {arquivo_destino}')
 	except PermissionError:
 		print(f'Sem Permissão para escrever em {arquivo_destino}')
@@ -26,7 +34,7 @@ def processar_arquivo(arquivo_origem,arquivo_destino):
 def main():
 	diretorio_trabalho = 'diretorio_trabalho'
 	arquivo_origem = os.path.join(diretorio_trabalho,'arquivo_origem.txt')
-	arquivo_destino = os.path.join(diretorio_trabalho,'arquivo_destino.txt')
+	arquivo_destino = os.path.join(diretorio_trabalho,'arquivo_destino.csv')
 	
 	processar_arquivo(arquivo_origem, arquivo_destino)
 
